7ed9a9aaf0c7c8af6f53a172c6d5664df7c01375
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-02 18:15:36 +0100

    added file main.js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-const card = document.querySelector('.flip-card');
+const card = document.getElementById('card');
 console.log(card);
+card.addEventListener('click', event => event.target.classList.add('.flip-card-inner'));
+
