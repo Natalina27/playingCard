81d1b7c89265a405b98d44bbdb70dd5f261d0722
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-06 21:02:28 +0100

    added eventListener for card

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const card = document.getElementById('card');
+let frontCard = document.querySelector('.flip-card-front');
+let backCard = document.querySelector('.flip-card-back');
 console.log(card);
-card.addEventListener('click', event => event.target.classList.add('.flip-card-inner'));
-
+console.log(frontCard);
+console.log(backCard);
+console.log(document.body.children);
+const onClick = ()  => {
+    (card.className === 'flip-card-inner' )?
+        card.classList.remove('flip-card-inner') :
+        card.classList.add('flip-card-inner');
+};
+card.classList.add('flip-card-inner');
+card.addEventListener('click', onClick);
