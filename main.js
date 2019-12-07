c4449e1cc2a3e4a236fd157394c90cae27898ede
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-07 10:05:41 +0100

    canceled unused code

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const card = document.getElementById('card');
-let frontCard = document.querySelector('.flip-card-front');
-let backCard = document.querySelector('.flip-card-back');
-console.log(card);
-console.log(frontCard);
-console.log(backCard);
-console.log(document.body.children);
 const onClick = ()  => {
     (card.className === 'flip-card-inner' )?
         card.classList.remove('flip-card-inner') :
         card.classList.add('flip-card-inner');
 };
-card.classList.add('flip-card-inner');
 card.addEventListener('click', onClick);
